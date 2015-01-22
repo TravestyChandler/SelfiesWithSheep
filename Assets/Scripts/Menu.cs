fb06b4c5a95aeef5840f4236a4ad526d752e7d4e
brandon999 <brandon@philadelphiagamelab.org>
2015-01-21 21:02:09 -0500

    Added the main menu in the beginning with images and everythang.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+	public GameObject tweetObject;
+	public GameObject panel1, panel2, panelOptions;
+	private Image tweet;
+	private bool doneFading;
+
+	// Use this for initialization
+	void Start () {
+		doneFading = false;
+		tweet = tweetObject.GetComponent<Image>();
+		FireFadeCoroutine(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(doneFading) {
+			panel1.SetActive (true);
+			panel2.SetActive(true);
+		}
+	}	
+
+	void FireFadeCoroutine(bool d) {
+		StartCoroutine(FadeTweet (d));
+	}
+
+	IEnumerator FadeTweet(bool d) {
+		for(float f=0f; f <=1.0f; f+=0.02f) {
+			Color c = tweet.color;
+			c.a = f;
+			tweet.color = c;
+			yield return new WaitForSeconds(0.05f);
+		}
+		yield return new WaitForSeconds(1.5f);
+		for(float f=1f; f >=0.0f; f-=0.02f) {
+			Color c = tweet.color;
+			c.a = f;
+			tweet.color = c;
+			yield return new WaitForSeconds(0.02f);
+		}
+		yield return new WaitForSeconds(1.5f);
+		doneFading = true;
+	}
+
+	public void OnCickStart() {
+		Application.LoadLevel (1);
+	}
+	
+	public void OnClickOptions() {
+		panel1.SetActive (false);
+		panelOptions.SetActive(true);
+	}
+	
+	public void OnClickBack() {
+		panelOptions.SetActive(false);
+		panel1.SetActive (true);
+	}
+}
---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnCickStart() {
+		Application.LoadLevel (1);
+	}
+
+	public void OnClickExit() {
+		Application.Quit ();
+	}
+}
